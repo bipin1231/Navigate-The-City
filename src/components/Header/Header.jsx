c0a617de99d8b3302b4ec35962d8e688dbe1b4d4
bipin1231 <bipinadhikari332@gmail.com>
2024-05-04 21:10:42 +0545

    new changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,19 @@
-import React, {useState, useEffect, useRef} from "react";
-import { Link } from "react-router-dom";
-import ProfileSidebar from "../SideBar/ProfileSidebar";
+import React from 'react'
+import {Link,NavLink} from 'react-router-dom'
+import {Button} from "@nextui-org/react";
 
-  const Header = () => {
-
-  const [leftSidebarOpen, setLeftSidebarOpen] = useState(false);
-  const toggleProfileSidebar = () => {
-    setLeftSidebarOpen(!leftSidebarOpen);
-  };
-  const closeLeftSidebar = () => {
-    setLeftSidebarOpen(false);
-  };
-
+function Header() {
   return (
-    <>
-      <div className="flex justify-start w-full bg-transparent fixed pointer-events-none z-[1000] top-3 ml-10">
-        <div 
-          className="cursor-pointer pointer-events-auto hover:scale-125 duration-200"
-          onClick={() => toggleProfileSidebar()}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        </div>
-        <ProfileSidebar 
-        isOpen={leftSidebarOpen}
-        onClose={closeLeftSidebar}/>
-
-        <Link to="/">
-          <div className="w-52  text-2xl justify-center items-center flex cursor-pointer pointer-events-auto">
-            Navigate
-          </div>
+    <nav className='bg-gray-950 text-white flex items-center h-20 text-xl'>
+     <Link to="/">
+    <div className='w-52  text-3xl justify-center items-center flex cursor-pointer font-semibold'> 
+      Navigate
+    </div>
+    </Link>
+    <div className='flex w-[60%] justify-center'>
+      <ul className='flex gap-16 font-medium text-xl'>
+        <Link to="/home">
+        <li className='cursor-pointer'>Home</li>
         </Link>
         <Link to="/searchbus">
         <li className='cursor-pointer'>Buy Ticket</li></Link>
@@ -56,11 +28,6 @@ import ProfileSidebar from "../SideBar/ProfileSidebar";
     </Link>
    </nav>
   )
-        
-      </div>
-    </>
-  );
 }
 
-
-export default Header;
+export default Header
