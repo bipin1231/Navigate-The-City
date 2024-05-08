6b0911ae97a22082b179ea44bab0ba56feb46dd2
Binam001 <binamshrestha770@gmail.com>
2024-05-08 12:20:21 +0545

    added logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,10 +16,10 @@ function Header() {
   };
 
   return (
-    <nav className='bg-gray-950 text-white flex items-center h-20 text-xl'>
+    <nav className='bg-[#1506B5] text-white flex items-center h-20 text-xl'>
       <Link to='/'>
         <div className='w-52  text-3xl justify-center items-center flex cursor-pointer font-semibold'>
-          Navigate
+          <img src='../logo.svg' className='w-25 h-12'/>
         </div>
       </Link>
       <div className='flex w-[60%] justify-center'>
