9d89e81d6689b771a7c7ba00d79c542fcb835ff1
bipin1231 <bipinadhikari332@gmail.com>
2024-05-29 21:20:57 +0545

    still trying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/collegeproject/",
+  base:"/6th-Sem-Project/",
   plugins: [react()],
 })
