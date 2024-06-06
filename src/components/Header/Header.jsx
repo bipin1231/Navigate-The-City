fb28c1ad1a8b0e4262ee0aed668f8b678dc91415
Binam001 <binamshrestha770@gmail.com>
2024-06-06 19:03:27 +0545

    changes in css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -106,7 +106,7 @@ getData();
   };
 
   return (
-    <nav className='bg-[#1506B5] text-white flex items-center h-[10vh] text-xl'>
+    <nav className='bg-[#1506B5] text-white flex items-center h-[10vh] text-xl w-full'>
       <Link to='/'>
         <div className='w-52  text-3xl justify-center items-center flex cursor-pointer font-semibold'>
           <img src='../logo.svg' className='w-25 h-12'/>
