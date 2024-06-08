591148bca3c00254401ae2bde932d6da9b7732f0
bipin1231 <bipinadhikari332@gmail.com>
2024-06-08 18:53:20 +0545

    added new features and current user different icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,8 @@ function Home() {
 
   return (
     <div>
-<MultipleUserMap/>
+   
+<MultipleUserMap/> 
       
     </div>
   );
--- src/components/Map/MultipleUserMap.jsx
+++ src/components/Map/MultipleUserMap.jsx
@@ -6,11 +6,15 @@ import L from "leaflet"
 import customIconUrl from "./batman.png"
 import service from '../../appwrite/config';
 import { useSelector } from 'react-redux';
+import MapWithCustomMaker from './MapWithCustomMarker';
 
 function MultipleUserMap() {
   const status=useSelector(state=>state.auth.status)
   const userData=useSelector(state=>state.auth.userData)
   const [users, setUsers] = useState([]);
+  const [position,setPosition]=useState(null);
+
+  const [currentUser,setCurrentUser]=useState("");
 
   useEffect(() => {
     const fetchUserLocation = async () => {
@@ -30,9 +34,6 @@ function MultipleUserMap() {
 
   }, [])
 
-  const [position,setPosition]=useState(null);
-
-if(status){  
 
 
 useEffect(() => {
@@ -41,9 +42,11 @@ useEffect(() => {
       (position) => {
         const { latitude, longitude } = position.coords;
         setPosition([latitude, longitude]);
+        if(status){
        const storeLoc=async()=>{ 
         const data=await service.storeUserLocation({userId:userData.$id,latitude,longitude});}
         storeLoc();
+       }
       },
       (error) => {
         console.error('Error occurred while retrieving location:', error);
@@ -59,11 +62,30 @@ useEffect(() => {
   }
 }, []);
 
-}
+// else{
+//   if (navigator.geolocation) {
+//     const geoId = navigator.geolocation.watchPosition(
+//       (position) => {
+//         const { latitude, longitude } = position.coords;
+//         setPosition([latitude, longitude]);
+//     console.log(position);
+//       },
+//       { enableHighAccuracy: true }
+//     );
+
+//     return () => {
+//       navigator.geolocation.clearWatch(geoId);
+//     };
+//   } else {
+//     console.error('Geolocation is not supported by this browser.');
+//   }
+
+// }
+
   const markerIcon = new L.Icon({
 
     // iconUrl:customIconUrl,
-    iconUrl: "https://th.bing.com/th/id/OIP.RVrT1OISI4rmKKMxADYN4wHaHa?rs=1&pid=ImgDetMain",
+    iconUrl: status?"https://th.bing.com/th/id/OIP.RVrT1OISI4rmKKMxADYN4wHaHa?rs=1&pid=ImgDetMain":"https://th.bing.com/th/id/OIP.hHv2ZGCBFMcuefjrIrjM5gHaI8?rs=1&pid=ImgDetMain",
     iconSize: [35, 45],
     iconAnchor: [17, 46],
     popupAnchor: [3, -46]
@@ -83,13 +105,15 @@ useEffect(() => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {
-        users.map(a => (<Marker key={a.userId} position={a.position} icon={markerIcon}>
+        users.map(a => (
+        <Marker key={a.userId} position={a.position} icon={markerIcon}>
           <Popup>
             Hello World. <br /> Easily customizable.<br />{a.userId}
           </Popup>
         </Marker>
         ))
       }
+    {!status &&   <MapWithCustomMaker/>}
     </MapContainer>
 
   )
