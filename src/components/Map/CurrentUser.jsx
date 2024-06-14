4be51beecb34defb46065e1c1902a2d2adb8507b
Binam001 <binamshrestha770@gmail.com>
2024-06-14 11:54:38 +0545

    adjusting current user icon

--- src/components/Map/CurrentUser.jsx
+++ src/components/Map/CurrentUser.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import customIconUrl from "../location.svg";
+// import customIconUrl from "./batman.png";
 
 function CurrentUser() {
   const [userPosition, setUserPosition] = useState(null);
   const [userDirection, setUserDirection] = useState(0);
 
-  // const markerIcon = new L.Icon({
-  //   iconUrl: customIconUrl,
-  //   iconSize: [35, 45],
-  //   iconAnchor: [17, 46],
-  //   popupAnchor: [3, -46],
-  // });
+  const markerIcon = new L.DivIcon({
+    html: `
+      <div class="transform transition-transform duration-200" style="transform: rotate(${userDirection}deg);">
+        <img src="../location.svg" class="w-9 h-11 border-none bg-transparent outline-none" />
+      </div>
+    `,
+    className: 'custom-marker', // Custom class for additional styling
+    iconSize: [35, 45],
+    iconAnchor: [17, 46],
+  });
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -53,15 +57,9 @@ function CurrentUser() {
     }
   }, []);
 
-  const iconWithRotation = L.divIcon({
-    html: `<img src="${customIconUrl}" style="width: 35px; height: 45px; transform: rotate(${userDirection}deg);" />`,
-    iconSize: [35, 45],
-    iconAnchor: [17, 46],
-  });
-
   return (
     userPosition && (
-      <Marker key={'001'} position={userPosition} icon={iconWithRotation}>
+      <Marker key={'001'} position={userPosition} icon={markerIcon}>
         <Popup>
           Current User Hello World. <br /> Easily customizable.
         </Popup>
