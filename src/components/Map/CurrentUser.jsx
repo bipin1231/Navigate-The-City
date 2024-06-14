792455251795b622eee232dc8838f95f02df85bb
Binam001 <binamshrestha770@gmail.com>
2024-06-14 12:43:39 +0545

    changes the device callibration and direction logics

--- src/components/Map/CurrentUser.jsx
+++ src/components/Map/CurrentUser.jsx
@@ -2,19 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-// import customIconUrl from "./batman.png";
 
 function CurrentUser() {
   const [userPosition, setUserPosition] = useState(null);
   const [userDirection, setUserDirection] = useState(0);
 
   const markerIcon = new L.DivIcon({
+    className: 'custom-marker',
     html: `
       <div class="transform transition-transform duration-200" style="transform: rotate(${userDirection}deg);">
         <img src="../location.svg" class="w-9 h-11 border-none bg-transparent outline-none" />
       </div>
     `,
-    className: 'custom-marker', // Custom class for additional styling
     iconSize: [35, 45],
     iconAnchor: [17, 46],
   });
@@ -43,14 +42,31 @@ function CurrentUser() {
   useEffect(() => {
     if (window.DeviceOrientationEvent) {
       const handleOrientation = (event) => {
-        const compassHeading = event.webkitCompassHeading || Math.abs(event.alpha - 360);
+        let compassHeading;
+        if (event.absolute) {
+          compassHeading = event.alpha;
+        } else if (event.webkitCompassHeading) {
+          compassHeading = event.webkitCompassHeading; // For Safari
+        } else {
+          compassHeading = Math.abs(event.alpha - 360); // For other browsers
+        }
         setUserDirection(compassHeading);
       };
 
+      window.addEventListener('deviceorientationabsolute', handleOrientation, true);
       window.addEventListener('deviceorientation', handleOrientation, true);
 
+      const handleCompassNeedsCalibration = (event) => {
+        alert('Your compass needs calibrating! Please wave your device in a figure-eight motion.');
+        event.preventDefault();
+      };
+
+      window.addEventListener('compassneedscalibration', handleCompassNeedsCalibration, true);
+
       return () => {
+        window.removeEventListener('deviceorientationabsolute', handleOrientation, true);
         window.removeEventListener('deviceorientation', handleOrientation, true);
+        window.removeEventListener('compassneedscalibration', handleCompassNeedsCalibration, true);
       };
     } else {
       console.error('Device orientation is not supported by this browser.');
