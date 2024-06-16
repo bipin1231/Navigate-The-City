8db7a1b49683b37188f5544271f00a11cc2e0d1e
Binam001 <binamshrestha770@gmail.com>
2024-06-16 10:06:43 +0545

    changes in css

--- src/components/User/CompanyInfo.jsx
+++ src/components/User/CompanyInfo.jsx
@@ -6,17 +6,17 @@ function CompanyInfo() {
       {/* company profile div */}
 			<div className='flex justify-center text-[25px] md:text-[35px]'>Company Details</div>
       <div className='flex justify-center'>
-        <span className='text-[25px]'>
+        <span className='text-[18px] md:text-[25px]'>
           ABC Company
         </span>
       </div>
       {/* company details div */}
-      <div className="grid grid-cols-2 gap-4 m-1 md:m-6">
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Owner Name: Prajwal K.C.</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Address: Pulchowk</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Contact: 9822222222</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>No. of Vehicles: 32</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>No. of Drivers: 30</div>
+      <div className="grid grid-cols-2 gap-1 mt-3 md:m-5">
+        <div className='text-sm md:text-lg'>Owner Name: Prajwal K.C.</div>
+        <div className='text-sm md:text-lg'>Address: Pulchowk</div>
+        <div className='text-sm md:text-lg'>Contact: 9822222222</div>
+        <div className='text-sm md:text-lg'>No. of Vehicles: 32</div>
+        <div className='text-sm md:text-lg'>No. of Drivers: 30</div>
       </div>
     </div>
   )
--- src/components/User/DriverInfo.jsx
+++ src/components/User/DriverInfo.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 
 function DriverInfo() {
   return (
-    <div className='pt-3 px-4'>
-      {/* driver profile div */}
-			<div className='flex justify-center text-[25px] md:text-[35px]'>Driver Info</div>
-      <div className='flex justify-center'>
-        <span className='text-[80px]'>
-          <ion-icon name="person-circle-outline"></ion-icon>
-        </span>
-      </div>
-      {/* driver details div */}
-      <div className="grid grid-cols-2 gap-4 mx-1 md:mx-6">
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Driver Name: Prajwal K.C.</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>License No.: 1248358</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Address: Pulchowk</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Contact: 9822222222</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Age: 32</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Vehicle No.: 01 Ba 11 3912</div>
-        <div className='w-[90%] text-sm md:text-lg border border-gray-400 p-2'>Exp: 5 years</div>
+    <div className='pt-2 px-4'>
+          <div className='flex justify-center pb-3 text-[25px] md:text-[35px]'>
+            Driver Profile
+          </div>
+      <div className='flex flex-col md:flex-row items-center justify-around'>
+        <div className=' w-[40%]'>
+          {/* driver profile div */}
+          <div className='flex justify-center'>
+            <span className='text-[80px]'>
+              <ion-icon name="person-circle-outline"></ion-icon>
+            </span>
+          </div>
+        </div>
+        <div className='md:w-[60%]'>
+          {/* driver details div */}
+          <div className="grid grid-cols-2 gap-1 mx-1 md:mx-6">
+            <div className='text-sm md:text-md'>Driver Name: Prajwal K.C.</div>
+            <div className='text-sm md:text-md'>License No.: 1248358</div>
+            <div className='text-sm md:text-md'>Address: Pulchowk</div>
+            <div className='text-sm md:text-md'>Contact: 9822222222</div>
+            <div className='text-sm md:text-md'>Age: 32</div>
+            <div className='text-sm md:text-md'>Vehicle No.: 01 Ba 11 3912</div>
+            <div className='text-sm md:text-md'>Exp: 5 years</div>
+          </div>
+        </div>
       </div>
     </div>
   );
