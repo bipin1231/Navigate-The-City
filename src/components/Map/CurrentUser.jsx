a4b7bc0cc4751e67dc093768f3467bb44db446fe
Binam001 <binamshrestha770@gmail.com>
2024-09-04 17:53:08 +0545

    removed multiple choice custom marker icon on currentUser file

--- src/components/Map/CurrentUser.jsx
+++ src/components/Map/CurrentUser.jsx
@@ -7,8 +7,6 @@ import Speedometer from './Speedometer';
 function CurrentUser() {
   const [userPosition, setUserPosition] = useState(null);
   const [userDirection, setUserDirection] = useState(0);
-  const [selectedIcon, setSelectedIcon] = useState('icon1');
-  const [bottomPosition, setBottomPosition] = useState(0);
   const [speed, setSpeed] = useState(0);
 
   useEffect(() => {
@@ -37,13 +35,9 @@ function CurrentUser() {
       let compassHeading;
 
       if (event.webkitCompassHeading) {
-        // For Safari on iOS
         compassHeading = event.webkitCompassHeading;
       } else if (event.alpha !== null) {
-        // For other devices
         const alpha = event.alpha;
-        const beta = event.beta;
-        const gamma = event.gamma;
 
         if (typeof window.orientation !== 'undefined') {
           if (window.orientation === 0) {
@@ -59,7 +53,6 @@ function CurrentUser() {
           compassHeading = alpha;
         }
 
-        // Normalize the compass heading to a 0-360 range
         if (compassHeading < 0) {
           compassHeading += 360;
         }
@@ -67,7 +60,7 @@ function CurrentUser() {
           compassHeading -= 360;
         }
       } else {
-        compassHeading = 0; // Default to 0 if no orientation data is available
+        compassHeading = 0;
       }
 
       setUserDirection(compassHeading);
@@ -99,50 +92,25 @@ function CurrentUser() {
     };
   }, []);
 
-  const createCustomIcon = (selectedIcon, userDirection) => {
-    let iconUrl;
-    switch (selectedIcon) {
-      case 'icon1':
-        iconUrl = '../navigator.svg';
-        break;
-      case 'icon2':
-        iconUrl = '../car.svg';
-        break;
-      case 'icon3':
-        iconUrl = '../taxi.svg';
-        break;
-      case 'icon4':
-        iconUrl = '../bus.png';
-        break;
-      case 'icon5':
-        iconUrl = '../bike.svg';
-        break;
-      default:
-        iconUrl = '../navigator.svg';
-    }
-
-    return new L.DivIcon({
-      className: 'custom-marker',
-      html: `
-        <div class="marker-icon" style="transform: rotate(${360 - userDirection}deg);">
-          <img src="${iconUrl}" class="w-4 h-4 border-none bg-transparent outline-none" />
-        </div>
-      `,
-      iconSize: [15, 25],
-      popupAnchor: [0, -46]
-    });
-  };
-
-  const markerIcon = createCustomIcon(selectedIcon, userDirection);
-
-  const handleIconChange = (event) => {
-    setSelectedIcon(event.target.value);
-  };
-
   return (
     userPosition && (
       <div className='flex flex-col items-center'>
-        <Marker key={'001'} position={userPosition} icon={markerIcon}>
+        <Marker
+          key={'001'}
+          position={userPosition}
+          icon={
+            new L.DivIcon({
+              className: 'custom-marker',
+              html: `
+                <div class="marker-icon" style="transform: rotate(${360 - userDirection}deg);">
+                  <img src="../navigator.svg" class="w-4 h-4 border-none bg-transparent outline-none" />
+                </div>
+              `,
+              iconSize: [15, 25],
+              popupAnchor: [0, -46]
+            })
+          }
+        >
           <Popup>
             <Speedometer speed={speed} />
           </Popup>
