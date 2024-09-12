cef11af034ec3ec08b5d64e8a337f5babaa07aa3
Binam001 <binamshrestha770@gmail.com>
2024-09-12 10:12:00 +0545

    added all images on landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -51,7 +51,7 @@ function LandingPage() {
 
           <ScrollAnimation animateIn="fadeIn" duration={1.5} delay={200}>
             <div className='flex flex-col items-center p-6 rounded-lg shadow-lg'>
-              <img src="https://cms.trackon-gps.com/media/reasons/trackon-bus-2.webp" alt="" />
+              <img src="https://img.freepik.com/premium-vector/people-waiting-bus-stop-female-male-passengers-sitting-modern-station-man-standing-near-route-map_102902-7345.jpg?ga=GA1.2.284706500.1718984879&semt=ais_hybrid" alt="" />
               <h1 className='text-lg font-semibold'>Estimated Arrival</h1>
               <p>Never miss a bus or train. Get accurate ETA and plan your journey better</p>
             </div>
@@ -59,7 +59,7 @@ function LandingPage() {
 
           <ScrollAnimation animateIn="fadeIn" duration={1.5} delay={200}>
             <div className='flex flex-col items-center p-6 rounded-lg shadow-lg'>
-              <img src="https://cms.trackon-gps.com/media/reasons/trackon-bus-2.webp" alt="" />
+              <img src="https://img.freepik.com/free-vector/parents-looking-school-bus-location-pin-map-tablet-child-tracking-system-school-bus-route-child-safety-security-concious-parents-concept-vector-isolated-illustration_335657-1985.jpg?ga=GA1.2.284706500.1718984879&semt=ais_hybrid" alt="" />
               <h1 className='text-lg font-semibold'>Route Planning</h1>
               <p>Navigate your route with ease using our intuitive interface</p>
             </div>
@@ -76,30 +76,30 @@ function LandingPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           <ScrollAnimation animateIn="fadeInUp" duration={1.5}>
             <div className="bg-white rounded-lg shadow-lg p-8 h-72">
-              <div className="mb-6">
-                <img src="https://via.placeholder.com/80" alt="Benefit 1" className="mx-auto" />
+              <div className="mb-4">
+                <img src="https://img.freepik.com/premium-vector/people-auto-station-man-woman-standing-near-transport-waiting_625536-4298.jpg?ga=GA1.1.284706500.1718984879&semt=ais_hybrid" alt="Benefit 1" className="mx-auto w-20 h-20 object-cover" />
               </div>
-              <h3 className="text-2xl font-semibold mb-4">Convenience</h3>
+              <h3 className="text-2xl font-semibold mb-3">Convenience</h3>
               <p>Access all transportation options in one place, ensuring you always have the best route at your fingertips.</p>
             </div>
           </ScrollAnimation>
 
           <ScrollAnimation animateIn="fadeInUp" duration={1.5} delay={200}>
             <div className="bg-white rounded-lg shadow-lg p-8  h-72">
-              <div className="mb-6">
-                <img src="https://via.placeholder.com/80" alt="Benefit 2" className="mx-auto" />
+              <div className="mb-4">
+                <img src="https://cms.trackon-gps.com/media/reasons/trackon-bus-2.webp" alt="Benefit 2" className="mx-auto w-20 h-20 object-cover" />
               </div>
-              <h3 className="text-2xl font-semibold mb-4">Convenient and cost-effective</h3>
+              <h3 className="text-2xl font-semibold mb-3">Convenient and cost-effective</h3>
               <p>No additional equipment needed—access all features directly from your mobile device</p>
             </div>
           </ScrollAnimation>
 
           <ScrollAnimation animateIn="fadeInUp" duration={1.5} delay={400}>
             <div className="bg-white rounded-lg shadow-lg p-8 h-72">
-              <div className="mb-6">
-                <img src="https://via.placeholder.com/80" alt="Benefit 3" className="mx-auto" />
+              <div className="mb-4">
+                <img src="https://img.freepik.com/premium-vector/school-bus-tracking-system-abstract-concept-vector-illustration_107173-25549.jpg?ga=GA1.1.284706500.1718984879&semt=ais_hybrid" alt="Benefit 3" className="mx-auto w-20 h-20 object-cover" />
               </div>
-              <h3 className="text-2xl font-semibold mb-4">Reliability</h3>
+              <h3 className="text-2xl font-semibold mb-3">Reliability</h3>
               <p>Count on our real-time updates and accurate schedules to get you to your destination on time.</p>
             </div>
           </ScrollAnimation>
@@ -110,7 +110,7 @@ function LandingPage() {
       <section className="container mx-auto px-6 py-20 text-center bg-gray-50">
         <h2 className="text-3xl font-bold mb-8">Revolutionize Public Transportation</h2>
         <p className="text-lg lg:text-xl mb-8">Innovating the commuting experience for you. Stay connected and stay ahead with our intuitive platform.</p>
-        <img src="https://via.placeholder.com/500x300" alt="Public Transportation Image" className="w-full max-w-lg mx-auto rounded-lg shadow-md" />
+        <img src="https://img.freepik.com/free-vector/people-getting-bus-concept-illustration_114360-21778.jpg?ga=GA1.1.284706500.1718984879&semt=ais_hybrid" alt="Public Transportation Image" className="w-full max-w-lg mx-auto rounded-lg shadow-md" />
       </section>
 
       {/* Footer */}
